276fed07b2268e238921588fa6a58912c4b0c70b
Grigore Stefan <g_stefan@yahoo.com>
2023-01-10 12:58:24 +0200

    Update

--- source/XYO/Cryptography/License.cpp
+++ source/XYO/Cryptography/License.cpp
@@ -1,7 +1,7 @@
 // Cryptography
-// Copyright (c) 2022 Grigore Stefan <g_stefan@yahoo.com>
+// Copyright (c) 2016-2023 Grigore Stefan <g_stefan@yahoo.com>
 // MIT License (MIT) <http://opensource.org/licenses/MIT>
-// SPDX-FileCopyrightText: 2022 Grigore Stefan <g_stefan@yahoo.com>
+// SPDX-FileCopyrightText: 2016-2023 Grigore Stefan <g_stefan@yahoo.com>
 // SPDX-License-Identifier: MIT
 
 #include <XYO/Cryptography/License.hpp>
@@ -9,36 +9,31 @@
 namespace XYO::Cryptography::License {
 
 	static const char *license_ =
+	    "        MIT License\r\n"
 	    "\r\n"
-	    "XYO\r\n"
+	    "Copyright (c) 2016-2023 Grigore Stefan <g_stefan@yahoo.com>\r\n"
 	    "\r\n"
-	    "MIT License (MIT)\r\n"
-	    "\r\n"
-	    "Copyright (c) 2022 Grigore Stefan <g_stefan@yahoo.com>\r\n"
-	    "\r\n"
-	    "Permission is hereby granted, free of charge, to any person obtaining a copy of\r\n"
-	    "this software and associated documentation files (the \"Software\"), to deal in\r\n"
-	    "the Software without restriction, including without limitation the rights to\r\n"
-	    "use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of\r\n"
-	    "the Software, and to permit persons to whom the Software is furnished to do so,\r\n"
-	    "subject to the following conditions:\r\n"
+	    "Permission is hereby granted, free of charge, to any person obtaining a copy\r\n"
+	    "of this software and associated documentation files (the \" Software \"), to deal\r\n"
+	    "in the Software without restriction, including without limitation the rights\r\n"
+	    "to use, copy, modify, merge, publish, distribute, sublicense, and/or sell\r\n"
+	    "copies of the Software, and to permit persons to whom the Software is\r\n"
+	    "furnished to do so, subject to the following conditions:\r\n"
 	    "\r\n"
 	    "The above copyright notice and this permission notice shall be included in all\r\n"
 	    "copies or substantial portions of the Software.\r\n"
 	    "\r\n"
-	    "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\r\n"
-	    "IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS\r\n"
-	    "FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR\r\n"
-	    "COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER\r\n"
-	    "IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN\r\n"
-	    "CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.\r\n"
-	    "\r\n";
+	    "THE SOFTWARE IS PROVIDED \" AS IS \", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\r\n"
+	    "IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\r\n"
+	    "FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\r\n"
+	    "AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\r\n"
+	    "LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\r\n"
+	    "OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\r\n"
+	    "SOFTWARE.\r\n";
 
 	static const char *shortLicense_ =
-	    "\r\n"
-	    "Copyright (c) 2022 Grigore Stefan <g_stefan@yahoo.com>\r\n"
-	    "MIT License (MIT) <http://opensource.org/licenses/MIT>\r\n"
-	    "\r\n";
+	    "Copyright (c) 2016-2023 Grigore Stefan <g_stefan@yahoo.com>\r\n"
+	    "MIT License (MIT) <http://opensource.org/licenses/MIT>\r\n";
 
 	const char *license() {
 		return license_;
